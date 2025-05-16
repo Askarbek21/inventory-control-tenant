daf3bfd66e6aedd9a7c1b8377e2319a25e795ec5
Artikbaev Javlon <artjavlon@gmail.com>
2025-05-16 11:19:47 +0500

    changed transfer, stock, category, measurement

--- apps/items/models.py
+++ apps/items/models.py
@@ -9,7 +9,6 @@ from apps.suppliers.models import Supplier
 
 class Category(models.Model):
     category_name = models.CharField(max_length=200)
-    store = models.ForeignKey('stores.Store', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.category_name
@@ -20,10 +19,22 @@ class Category(models.Model):
         db_table = 'category'
 
 
+class Measurement(models.Model):
+    measurement_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.measurement_name
+
+    class Meta:
+        verbose_name_plural = "Measurements"
+        db_table = 'measurement'
+
+
 class Product(models.Model):
     product_name = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    store = models.ForeignKey('stores.Store', on_delete=models.CASCADE)
+    measurement = models.ManyToManyField(Measurement, through='MeasurementProduct',
+                                         related_name='measurements')
 
     def __str__(self):
         return self.product_name
@@ -34,26 +45,28 @@ class Product(models.Model):
         db_table = 'product'
 
 
-class Measurement(models.Model):
-    measurement_name = models.CharField(max_length=100)
-    store = models.ForeignKey('stores.Store', on_delete=models.CASCADE)
+class MeasurementProduct(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    measurement = models.ForeignKey(Measurement, on_delete=models.CASCADE)
+    number = models.FloatField(validators=[MinValueValidator(0.0)])
 
     def __str__(self):
-        return self.measurement_name
+        return f'{self.measurement.measurement_name}'
 
     class Meta:
-        verbose_name_plural = "Measurements"
-        db_table = 'measurement'
+        verbose_name_plural = "Measurement Arrived Products In Stock"
+        db_table = 'measurement_arrived_products_in_stock'
 
 
 class Stock(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    measurement = models.ManyToManyField(Measurement, through='MeasurementArrivedProductInStore',
-                                         related_name='measurements')
+
     date_of_arrived = models.DateTimeField(auto_now_add=True)
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    purchase_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    purchase_price_in_us = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    purchase_price_in_uz = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    exchange_rate = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     selling_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     min_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     history_of_prices = models.JSONField(default=dict)
@@ -67,28 +80,6 @@ class Stock(models.Model):
         verbose_name_plural = "Stocks"
         ordering = ['-date_of_arrived']
         db_table = 'stock'
-
-
-class MeasurementArrivedProductInStore(models.Model):
-    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
-    measurement = models.ForeignKey(Measurement, on_delete=models.CASCADE)
-
-    number = models.FloatField(validators=[MinValueValidator(0.0)])
-
-    def __str__(self):
-        return f'{self.measurement.measurement_name}'
-
-    class Meta:
-        verbose_name_plural = "Measurement Arrived Products In Stock"
-        db_table = 'measurement_arrived_products_in_stock'
-
-
-class ArrivedProduct(models.Model):
-    data = models.JSONField()
-
-    class Meta:
-        verbose_name_plural = "Arrived Products"
-        db_table = 'arrived_product'
 
 
 class DeletedItems(models.Model):
