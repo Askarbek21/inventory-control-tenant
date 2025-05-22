dab8d270a77d0f83a08865226e6731a3116d0db4
Artikbaev Javlon <artjavlon@gmail.com>
2025-05-22 15:08:48 +0500

    added expenses

---
+++ apps/expenses/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.viewsets import ModelViewSet
+
+from config.pagination import CustomPageNumberPagination
+from .filters import ExpensesFilter, CashInFlowNameFilter
+from .models import ExpenseName, CashInFlowName, Expense
+from .serializers import ExpenseNameSerializer, CashInFlowNameSerializer, ExpenseSerializer
+
+
+class ExpenseNameViewSet(ModelViewSet):
+    queryset = ExpenseName.objects.all()
+    serializer_class = ExpenseNameSerializer
+    pagination_class = CustomPageNumberPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = ExpensesFilter
+
+
+class CashInFlowNameViewSet(ModelViewSet):
+    queryset = CashInFlowName.objects.all()
+    serializer_class = CashInFlowNameSerializer
+    pagination_class = CustomPageNumberPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = CashInFlowNameFilter
+
+
+class ExpenseViewSet(ModelViewSet):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+    pagination_class = CustomPageNumberPagination
