4846f6d33a27b848b505e261cd3e597b19cb439a
Artikbaev Javlon <artjavlon@gmail.com>
2025-05-29 11:29:46 +0500

    added dashboard

---
+++ apps/dashboard/views.py
@@ -0,0 +1,33 @@
+from django.db.models import Sum
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ItemsDashboardSerializer
+from ..items.models import Product, Stock
+
+
+# Create your views here.
+
+class ItemsDashboardAPIView(APIView):
+    serializer_class = ItemsDashboardSerializer
+
+    def get(self, request):
+        stock = Stock.objects.select_related(
+            "product",
+            "store",
+            "supplier",
+        ).only(
+            'id', 'product',
+            'quantity', 'quantity_for_history',
+            'store', 'supplier', 'date_of_arrived'
+        )
+        total_product = stock.count()
+        info_products = (
+            stock.values('product__product_name', 'store__name').annotate(total_quantity=Sum('quantity'))
+        )
+        total_product_name = stock.values('product').aggregate()
+        print(total_product_name)
+        return Response({
+            "total_product": total_product,
+            "info_products": info_products,
+        })
