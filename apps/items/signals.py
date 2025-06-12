382847c247c70f7389916c5b6fb9b0d70b3df17f
Artikbaev Javlon <artjavlon@gmail.com>
2025-06-12 23:36:53 +0500

    added signals and fixed update bug

--- apps/items/signals.py
+++ apps/items/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, post_save
 from django.dispatch import receiver
 from django.forms.models import model_to_dict
 from .models import *
@@ -13,3 +13,12 @@ def archive_products_before_delete(sender, instance, **kwargs):
         model=sender,
         data=data
     )
+
+
+@receiver(post_save, sender=Product)
+def update_stock_total_volume(sender, instance, **kwargs):
+    stock = Stock.objects.filter(product=instance)
+    for st in stock:
+        if instance.kub is not None and st.quantity is not None:
+            st.total_volume = st.quantity * instance.kub
+            st.save()
