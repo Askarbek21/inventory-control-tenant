755908da5d5bf79843e43bfaf87300bf142ea37c
Askarbek21 <askrisnazarov@gmail.com>
2025-07-21 02:02:17 +0300

    loan service modified

--- apps/loans/services.py
+++ apps/loans/services.py
@@ -3,33 +3,51 @@ from .models import Loan, LoanPayment
 
 
 def apply_existing_overpayment(loan: Loan):
-    previous = (
+    overpayment_loans = (
         Loan.objects
         .filter(
             sponsor=loan.sponsor,
             currency=loan.currency,
             overpayment_unused__gt=0
         )
-        .order_by('-created_at')
-        .first()
+        .order_by('created_at')
     )
-    if not previous:
-        return 
-    apply_amount = min(loan.remaining_balance, previous.overpayment_unused)
-    if apply_amount > 0:
-        LoanPayment.objects.create(
-            loan=loan,
-            amount=apply_amount,
-            payment_method='Перечисление',
-            notes=f'Автоматическое перечисление от взаимы #{previous.id}'
+
+    unpaid_loans = (
+        Loan.objects
+        .filter(
+            sponsor=loan.sponsor,
+            currency=loan.currency,
+            is_paid=False
         )
-        loan.remaining_balance -= apply_amount
-        if loan.remaining_balance <= 0:
-            loan.remaining_balance = 0
-            loan.is_paid = True
-        loan.save(update_fields=['remaining_balance', 'is_paid'])
-        previous.overpayment_unused -= apply_amount
-        previous.save(update_fields=['overpayment_unused'])
+        .exclude(id__in=overpayment_loans.values_list('id', flat=True))
+        .order_by('created_at')
+    )
+
+    for overpaying_loan in overpayment_loans:
+        for unpaid_loan in unpaid_loans:
+            if overpaying_loan.overpayment_unused <= 0:
+                break
+
+            apply_amount = min(unpaid_loan.remaining_balance, overpaying_loan.overpayment_unused)
+
+            if apply_amount > 0:
+                LoanPayment.objects.create(
+                    loan=unpaid_loan,
+                    amount=apply_amount,
+                    payment_method='Перечисление',
+                    notes=f'Автоматическое перечисление от займа #{overpaying_loan.id}'
+                )
+
+                unpaid_loan.remaining_balance -= apply_amount
+                if unpaid_loan.remaining_balance <= 0:
+                    unpaid_loan.remaining_balance = 0
+                    unpaid_loan.is_paid = True
+                unpaid_loan.save(update_fields=['remaining_balance', 'is_paid'])
+
+                overpaying_loan.overpayment_unused -= apply_amount
+                overpaying_loan.save(update_fields=['overpayment_unused'])
+
 
 
 def apply_loan_payment(loan: Loan, amount, payment_method, notes=''):
@@ -45,11 +63,17 @@ def apply_loan_payment(loan: Loan, amount, payment_method, notes=''):
         )
 
         loan.remaining_balance -= to_apply
+        overpaid = 0
         if loan.remaining_balance <= 0:
+            overpaid = abs(loan.remaining_balance)
             loan.remaining_balance = 0
             loan.is_paid = True
             overpaid = amount - initial_remaining_balance
             if overpaid > 0:
                 loan.overpayment_unused += overpaid
         loan.save()
+
+
+        if overpaid > 0:
+            apply_existing_overpayment(loan)
         return new_payment
